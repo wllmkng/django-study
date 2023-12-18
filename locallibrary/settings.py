4f15776a76ae81d28a932e518c4bf66d31056195
Yi Wang <56184784+wllmkng@users.noreply.github.com>
2023-12-18 04:03:59 +0000

    [fix] INSTALLED_APPS

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "catalog.apps.CatalogConfig",
 ]
 
 MIDDLEWARE = [
@@ -100,9 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-    },
-    {
-        "NAME": "catalog.apps.CatalogConfig",
     },
 ]
 
