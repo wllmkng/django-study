d64279ecda9839e4d2e43465631be964f6ff06d8
Yi Wang <56184784+wllmkng@users.noreply.github.com>
2023-12-22 03:46:48 +0000

    main page

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
+from django.urls import path, include
 from catalog import views
 
 urlpatterns = [
-
+    path('', views.index, name='index'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
 
 # Create your views here.
+
+def index(request):
+    """
+    View function for home page of site.
+    """
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    
+    num_science_fiction_books = Book.objects.filter(genre__name__icontains='science fiction').count()
+    num_detective_books = Book.objects.filter(genre__name__icontains='detective').count()
+    
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+    
+    # Render the HTML template index.html with the data in the context variable
+    return render(
+        request,
+        'index.html',
+        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors, 'num_science_fiction_books':num_science_fiction_books, 'num_detective_books':num_detective_books},
+    )
